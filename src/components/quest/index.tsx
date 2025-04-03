bfcd5573710022417ee404819244ae689ab50334
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-04-03 03:04:54 -0300

    feat: add notifications popup and function

--- src/components/quest/index.tsx
+++ src/components/quest/index.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext, themes } from "../../contexts/ThemeContext";
 import { Difficulty, Quest, QuestStatus } from "../../types/questData";
 import { QuestContext } from "../../contexts/QuestContext";
@@ -24,6 +24,7 @@ export const QuestItem = ({ selectedTimeline, filterDifficulty, filterStatus, fi
     const { theme } = useContext(ThemeContext)
     const { deleteQuest, loading, setStatus } = useContext(QuestContext)
     const { getBalance } = useContext(JoysContext)
+    const [notifications, setNotifications] = useState<string[]>([])
 
     const createQuestForm = (questData: Quest) => {
         setEditQuestData(questData);
@@ -56,7 +57,7 @@ export const QuestItem = ({ selectedTimeline, filterDifficulty, filterStatus, fi
         const quest = filterQuery?.find(q => q.id === questId);
 
         if (!quest) return;
-    
+
         if (quest.status === newStatus || quest.status === 'COMPLETO' || quest.status === 'INCOMPLETO') return;
 
         if (newStatus === 'COMPLETO' || newStatus === 'INCOMPLETO') {
@@ -75,129 +76,176 @@ export const QuestItem = ({ selectedTimeline, filterDifficulty, filterStatus, fi
     const hasQuests = filterByTimeline && filterByTimeline.length > 0;
 
     const filteredDIfficultyQuests = filterDifficulty
-    ? filterByTimeline?.filter(quest => quest.difficulty === filterDifficulty)
-    : filterByTimeline;
+        ? filterByTimeline?.filter(quest => quest.difficulty === filterDifficulty)
+        : filterByTimeline;
 
     const filteredQuests = filterStatus
-    ? filteredDIfficultyQuests?.filter(quest => quest.status === filterStatus)
-    : filteredDIfficultyQuests;
+        ? filteredDIfficultyQuests?.filter(quest => quest.status === filterStatus)
+        : filteredDIfficultyQuests;
 
-    const transformDateToPtbr = (newDate: string | number ): string => {
+    const transformDateToPtbr = (newDate: string | number): string => {
         const dt = DateTime.fromJSDate(new Date(newDate)).setLocale('pt-BR')
 
         return dt.toFormat('dd/MM/yyyy - HH:mm')
     }
 
+    const checkQuestsTimeLimit = (quests: Quest[]) => {
+        const newNotifications: string[] = [];
+
+        quests?.forEach((quest) => {
+            const questTimeLimit = DateTime.fromISO(quest.validation); // Certifique-se que a data está no formato ISO
+            if (questTimeLimit < DateTime.now()) {
+                newNotifications.push(`A quest "${quest.title}" passou do tempo limite! Você concluiu?`);
+            }
+        });
+
+        return newNotifications  // Atualiza o estado para exibir notificações
+    }
+
+    useEffect(() => {
+        const newNotifications = checkQuestsTimeLimit(filteredQuests || [])
+
+        if (newNotifications.length > 0 && newNotifications.join() !== notifications.join()) {
+            setNotifications(newNotifications)
+        }
+    }, [filteredQuests])
+
+    const markAsRead = (index: number) => {        
+        const updatedNotifications = notifications.filter((_, i) => i !== index);
+        setNotifications(updatedNotifications);
+    };
+
+
     if (loading) return <div>Loading..</div>
 
 
     return (
-        <CardsContainer $black_to_white={themes[theme].black_to_white}>
-            {hasQuests ? (filteredQuests?.map(quest => (
-                <Card className="card" key={quest.id} $object={themes[theme].object} $black_to_white={themes[theme].black_to_white} $emphasize_more={themes[theme].emphasize_more} $emphasize_less={themes[theme].emphasize_less}>
+        <>
+            {notifications.length > 0 && <Overlay />}
+            {notifications.length > 0 && (
+                <Notifications>
                     <div className="header">
-                        <div className={`category ${quest.timeline}`}>
-                            <span className="material-symbols-outlined icon">
-                                calendar_clock
-                            </span>
-                            <p>{quest.timeline}</p>
-                        </div>
-                        <div className="options">
-                            <span className="material-symbols-outlined icon" onClick={() => updateMenu(quest.id)}>
-                                more_vert
-                            </span>
-                        </div>
+                        <h2 className="title">Notificações</h2>
+                        <p className="description">Todas suas notificações estarão aqui!</p>
                     </div>
-                    <div className="body">
-                        <h3 className="title">{quest.title}</h3>
-                        <p className="description">{quest.description}</p>
-                        <div className="limit afterDescription">
-                            <span className="material-symbols-outlined icon">
-                                calendar_clock
-                            </span>
-                            <p>Até: {transformDateToPtbr(quest.validation)}</p>
+                    {notifications.map((notification, index) => (
+                        <div key={index} className="notificationItem">
+                            <div className="message">{notification}</div>
+                            <div className="buttonContainer">
+                                <button onClick={() => {filteredQuests?.map(quest => quest.status === 'COMPLETO'); markAsRead(index)}}>Sim. Terminei.</button>
+                                <button className="failed" onClick={() => {filteredQuests?.map(quest => quest.status === 'INCOMPLETO'); markAsRead(index)}}>Não consegui.</button>
+                            </div>
                         </div>
-                        <div className="joys afterDescription">
-                            <div className="joysQuantity">
-                                <span className="material-symbols-outlined joyLogo">
-                                    paid
+                    ))}
+                </Notifications>
+            )}
+            <CardsContainer $black_to_white={themes[theme].black_to_white}>
+                {hasQuests ? (filteredQuests?.map(quest => (
+                    <Card className="card" key={quest.id} $object={themes[theme].object} $black_to_white={themes[theme].black_to_white} $emphasize_more={themes[theme].emphasize_more} $emphasize_less={themes[theme].emphasize_less}>
+                        <div className="header">
+                            <div className={`category ${quest.timeline}`}>
+                                <span className="material-symbols-outlined icon">
+                                    calendar_clock
                                 </span>
-                                <p className="joys">{quest.joys} Joys</p>
+                                <p>{quest.timeline}</p>
                             </div>
-                            <div className={`difficultyLevel ${quest.difficulty}`}>
-                                <p>{quest.difficulty}</p>
+                            <div className="options">
+                                <span className="material-symbols-outlined icon" onClick={() => updateMenu(quest.id)}>
+                                    more_vert
+                                </span>
                             </div>
                         </div>
-                    </div>
-                    <div className={`footer ${quest.status}`}>
-                        <div className="status" >
-                            <span className="circleProgress"></span>
-                            <p>{quest.status}</p>
+                        <div className="body">
+                            <h3 className="title">{quest.title}</h3>
+                            <p className="description">{quest.description}</p>
+                            <div className="limit afterDescription">
+                                <span className="material-symbols-outlined icon">
+                                    calendar_clock
+                                </span>
+                                <p>Até: {transformDateToPtbr(quest.validation)}</p>
+                            </div>
+                            <div className="joys afterDescription">
+                                <div className="joysQuantity">
+                                    <span className="material-symbols-outlined joyLogo">
+                                        paid
+                                    </span>
+                                    <p className="joys">{quest.joys} Joys</p>
+                                </div>
+                                <div className={`difficultyLevel ${quest.difficulty}`}>
+                                    <p>{quest.difficulty}</p>
+                                </div>
+                            </div>
                         </div>
-                        <button className="setStatus" onClick={() => updateStatusMenu(quest.id)}><span className="material-symbols-outlined icon">check_circle</span>Status</button>
-                    </div>
+                        <div className={`footer ${quest.status}`}>
+                            <div className="status" >
+                                <span className="circleProgress"></span>
+                                <p>{quest.status}</p>
+                            </div>
+                            <button className="setStatus" onClick={() => updateStatusMenu(quest.id)}><span className="material-symbols-outlined icon">check_circle</span>Status</button>
+                        </div>
 
 
-                    {activeMenuId === quest.id &&
-                        <EditPopup $background={themes[theme].background} $black_to_white={themes[theme].black_to_white}>
-                            <div onClick={() => deleteQuest(quest.id)}>
-                                <span className="material-symbols-outlined deleteIcon icon">
-                                    delete
-                                </span>
-                                <button className="delete-btn btn">Deletar</button>
-                            </div>
-                            <div onClick={() => createQuestForm(quest)}>
-                                <span className="material-symbols-outlined editIcon icon">
-                                    edit_square
-                                </span>
-                                <button className="edit-btn btn">Editar</button>
-                            </div>
-                        </EditPopup>
-                    }
-                    {open && <Overlay onClick={() => closeCreateForm()} />}
-                    {open && activeMenuId === quest.id && (
-                        <QuestForm
-                            onClose={() => setOpen(false)}
-                            mode="edit"
-                            initialData={editQuestData}
-                        />
-                    )}
-                    {openStatus === quest.id && (
-                        <StatusPopup $background={themes[theme].background} $black_to_white={themes[theme].black_to_white}>
-                            <div onClick={() => changeStatus(quest.id, 'NULO')}>
-                                <span className="material-symbols-outlined icon">
-                                    radio_button_unchecked
-                                </span>
-                                <button className="incomplete-btn status-incomplete">Not Started</button>
-                            </div>
-                            <div onClick={() => changeStatus(quest.id, 'PENDENTE')}>
-                                <span className="material-symbols-outlined icon">
-                                    schedule
-                                </span>
-                                <button className="pending-btn status-pending">In Progress</button>
-                            </div>
-                            <div onClick={() => changeStatus(quest.id, 'COMPLETO')}>
-                                <span className="material-symbols-outlined icon">
-                                    check_circle
-                                </span>
-                                <button className="complete-btn status-complete">Completed</button>
-                            </div>
-                            <div onClick={() => changeStatus(quest.id, 'INCOMPLETO')}>
-                                <span className="material-symbols-outlined icon">
-                                    close
-                                </span>
-                                <button className="complete-btn status-complete">Failed</button>
-                            </div>
-                        </StatusPopup>
-                    )
-                    }
-                </Card>
-            ))) : (
-                <span className="warn">Não há quests para o filtro selecionado.</span>
-            )
-            }
+                        {activeMenuId === quest.id &&
+                            <EditPopup $background={themes[theme].background} $black_to_white={themes[theme].black_to_white}>
+                                <div onClick={() => deleteQuest(quest.id)}>
+                                    <span className="material-symbols-outlined deleteIcon icon">
+                                        delete
+                                    </span>
+                                    <button className="delete-btn btn">Deletar</button>
+                                </div>
+                                <div onClick={() => createQuestForm(quest)}>
+                                    <span className="material-symbols-outlined editIcon icon">
+                                        edit_square
+                                    </span>
+                                    <button className="edit-btn btn">Editar</button>
+                                </div>
+                            </EditPopup>
+                        }
+                        {open && <Overlay onClick={() => closeCreateForm()} />}
+                        {open && activeMenuId === quest.id && (
+                            <QuestForm
+                                onClose={() => setOpen(false)}
+                                mode="edit"
+                                initialData={editQuestData}
+                            />
+                        )}
+                        {openStatus === quest.id && (
+                            <StatusPopup $background={themes[theme].background} $black_to_white={themes[theme].black_to_white}>
+                                <div onClick={() => changeStatus(quest.id, 'NULO')}>
+                                    <span className="material-symbols-outlined icon">
+                                        radio_button_unchecked
+                                    </span>
+                                    <button className="incomplete-btn status-incomplete">Not Started</button>
+                                </div>
+                                <div onClick={() => changeStatus(quest.id, 'PENDENTE')}>
+                                    <span className="material-symbols-outlined icon">
+                                        schedule
+                                    </span>
+                                    <button className="pending-btn status-pending">In Progress</button>
+                                </div>
+                                <div onClick={() => changeStatus(quest.id, 'COMPLETO')}>
+                                    <span className="material-symbols-outlined icon">
+                                        check_circle
+                                    </span>
+                                    <button className="complete-btn status-complete">Completed</button>
+                                </div>
+                                <div onClick={() => changeStatus(quest.id, 'INCOMPLETO')}>
+                                    <span className="material-symbols-outlined icon">
+                                        close
+                                    </span>
+                                    <button className="complete-btn status-complete">Failed</button>
+                                </div>
+                            </StatusPopup>
+                        )
+                        }
+                    </Card>
+                ))) : (
+                    <span className="warn">Não há quests para o filtro selecionado.</span>
+                )
+                }
 
-        </CardsContainer >
+            </CardsContainer >
+        </>
     )
 }
 
@@ -210,6 +258,69 @@ const Overlay = styled.div`
     background-color: rgba(0, 0, 0, 0.5); 
     z-index: 4; 
     pointer-events: all; 
+`
+
+const Notifications = styled.div`
+    width: 100%;
+    max-width: 700px;
+    height: 100%;
+    max-height: 80%;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    user-select: none;
+    background-color: var(--background);
+
+    .header{
+        margin-bottom: 20px;
+        display: flex;
+        flex-direction: column;
+    }
+    .notificationItem{
+        background-color: var(--light-background);
+        color: white;
+        padding: 20px;
+        gap: 10px;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-bottom: 20px;
+        border-radius: 10px;
+    }
+    .notificationItem .buttonContainer{
+        width: 100%;
+        display: flex;
+        margin-top: 20px;
+    }
+    .notificationItem .buttonContainer button{
+        padding: 8px 12px;
+        width: 100%;
+        background: var(--tertiary);
+        border-radius: 5px;
+        color: white;
+        font-weight: 700;
+        margin-right: 10px;
+        border: none;
+        cursor: pointer;
+        transition: 0.15s ease-out;
+        &:hover{
+            background: green;
+        }
+    }
+    .notificationItem .buttonContainer button.failed{
+        background: transparent;
+        border: 1px solid var(--tertiary);
+        &:hover{
+            background: red;
+            border: 1px solid transparent;
+        }
+    }
 `
 
 const CardsContainer = styled.section<{ $black_to_white: string }>`
