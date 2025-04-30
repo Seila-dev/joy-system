89ebed89531e7c7de5278eac1953460a184b1132
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-04-30 00:00:51 -0300

    feat: created habitnotfound component

--- src/components/habitPage/index.tsx
+++ src/components/habitPage/index.tsx
@@ -9,9 +9,13 @@ export const HabitPage = () => {
                         <p className="description">Adicione, gerencie e acompanhe hábitos que você quer melhorar ou remover da sua vida de uma vez por todas.</p>
                     </div>
                     <div className="rightSide">
-                        <button className="addQuest btn cta"><span className="material-symbols-outlined icon">add</span> <span className="removeResponsive">Nova tarefa</span></button>
+                        <button className="addQuest btn cta"><span className="material-symbols-outlined icon">add</span> <span className="removeResponsive">Novo hábito</span></button>
                     </div>
                 </Introduction>
+                <HabitNotfound>
+                    <h1 className="title">Nenhum hábito encontrado</h1>
+                    <p className="description">Adicione seu primeiro hábito para começar sua jornada.</p>
+                </HabitNotfound>
         </Container>
     )
 }
@@ -35,7 +39,7 @@ const Introduction = styled.header`
     align-items: flex-start;
     width: 100%;
     .description{
-        margin-right: 20px;
+        margin: 5px 20px 0 0;
     }
 
     .rightSide{
@@ -44,22 +48,16 @@ const Introduction = styled.header`
         margin-top: 10px;
         gap: 15px;
     }
-    .rightSide .joysStore{
-        background: var(--secondary);
-        padding: 8px;
-        border-radius: 5px;
-    }
     .btn{
         padding: 12px 16px;
         border: none;
         cursor: pointer;
         display: flex;
         align-items: center;
-        width: 130px;
+        width: fit-content;
         border-radius: 5px;
-        background: var(--background);
-        background: var(--secondary);
-        color: white;
+        color: var(--background);
+        background: var(--tertiary);
         font-weight: 700;
     }
     .btn .icon{
@@ -82,5 +80,20 @@ const Introduction = styled.header`
             margin-top: 10px;
             width: 100%;
         }
+    }
+`
+
+const HabitNotfound = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 50vh;
+    opacity: 0.6;
+    text-align: center;
+
+    .description{
+        margin: 5px 0 0 0;
     }
 `
